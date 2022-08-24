f359c5c67607359cf5934226893b46aaa018d943
Михаил <rotari733@gmail.com>
2022-08-24 23:02:38 +0300

    init

--- src/main/java/ru/rotar/JavaProjectAlishev.java
+++ src/main/java/ru/rotar/JavaProjectAlishev.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class FirstRestAppApplication {
+public class JavaProjectAlishev {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstRestAppApplication.class, args);
+		SpringApplication.run(JavaProjectAlishev.class, args);
 	}
 @Bean
 	public ModelMapper modelMapper() {
